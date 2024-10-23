70f8b6d0734f9b195feadc33cc89a8b2dda68df3
lserhat22 <100235011+lserhat22@users.noreply.github.com>
2024-10-23 08:37:14 +0300

    Update BaseSteps.java

--- src/main/java/com/kitapyurdu/BaseSteps.java
+++ src/main/java/com/kitapyurdu/BaseSteps.java
@@ -2,7 +2,7 @@
 public class BaseSteps {
     void bug(){
         for (int i = 2; i < 0; i++) {
-            System.out.println("Teknasyon");
+            System.out.println("Teknasyon"); //test
         }
     }
 }
